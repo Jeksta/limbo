7f711f94b222e5ce0e0e0fc9ac38a4f937470838
Jeksta <38587116+Jeksta@users.noreply.github.com>
2024-08-18 16:58:04 +0200

    Delete out/build/debug directory

--- out/build/debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Project/lexer
-# Build directory: D:/Project/lexer/out/build/debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
